b23fc0f9d6fade0a69ec1d1dd9201cd9ccb32341
SilverQ <scor9947@hanmail.net>
2019-04-14 19:47:35 +0900

    using tensorboard, scope, etc

--- lab12-1.py
+++ lab12-1.py
@@ -1,15 +1,26 @@
 import tensorflow as tf
 import numpy as np
 
-hidden_size = 2
 
-cell = tf.nn.rnn_cell.LSTMCell(num_units=hidden_size)
+def vanila_rnn():
+    hidden_size = 2
 
-x_data = np.array([[[1, 0, 0, 0]]], dtype=np.float32)
+    cell = tf.nn.rnn_cell.LSTMCell(num_units=hidden_size)
 
-outputs, _states = tf.nn.dynamic_rnn(cell, x_data, dtype=tf.float32)
+    x_data = np.array([[[1, 0, 0, 0]]], dtype=np.float32)
 
+    outputs, _states = tf.nn.dynamic_rnn(cell, x_data, dtype=tf.float32)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        outputs_val = sess.run(outputs)
+        print(outputs_val)
+
+# lab12-2의 sequence_loss
+y_data = tf.constant([[1, 1, 1]])
+pred = tf.constant([[[0.2, 0.7], [0.6, 0.2], [0.2, 0.9]]], dtype=tf.float32)
+weight = tf.constant([[1, 1, 1]], dtype=tf.float32)
+
+# seq_loss = tf.contrib.seq2seq.sequence_loss(logits, targets, weights)
+seq_loss = tf.contrib.seq2seq.sequence_loss(logits=pred, targets=y_data, weights=weight)
 with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    outputs_val = sess.run(outputs)
-    print(outputs_val)
--- lab12-2.py
+++ lab12-2.py
@@ -3,3 +3,28 @@ import numpy as np
 
 # 입력 텍스트를 신경써서 가공해보자.
 # one hot encoding으로 바꿔주려면, vocabulary 생성, index가 필요
+
+input_word = 'hihello'
+id2char = ['h', 'i', 'e', 'l', 'o']
+x_data = [[0, 1, 0, 2, 3, 3]]
+x_onehot = [[[1, 0, 0, 0, 0],
+             [0, 1, 0, 0, 0],
+             [1, 0, 0, 0, 0],
+             [0, 0, 1, 0, 0],
+             [0, 0, 0, 1, 0],
+             [0, 0, 0, 1, 0]]]
+y_data = [[1, 0, 2, 3, 3, 4]]
+
+batch_size = 1
+seq_length = 6
+input_dim = 5
+hidden_size = 5
+X = tf.placeholder(dtype=tf.float32, shape=[1, seq_length, input_dim])
+Y = tf.placeholder(dtype=tf.float32, shape=[1, seq_length])
+
+rnn_cell = tf.nn.rnn_cell.BasicRNNCell(num_units=hidden_size)
+initial_state = rnn_cell.zero_state(batch_size=batch_size, dtype=tf.float32)
+
+# sequence_loss가 어떻게 동작하는지는 lab12-1에서 해보는 중
+outputs, _states = tf.nn.dynamic_rnn(rnn_cell, X, initial_state=initial_state, dtype=tf.float32)
+

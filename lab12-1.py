d2d4ab95c14b54a6adcf92f9d2b79b696f4952db
SilverQ <scor9947@hanmail.net>
2019-04-16 19:00:07 +0900

    understanding sequence_loss

--- lab12-1.py
+++ lab12-1.py
@@ -16,11 +16,17 @@ def vanila_rnn():
         outputs_val = sess.run(outputs)
         print(outputs_val)
 
+
 # lab12-2의 sequence_loss 해보는 중
 y_data = tf.constant([[1, 1, 1]])
-pred = tf.constant([[[0.2, 0.7], [0.6, 0.2], [0.2, 0.9]]], dtype=tf.float32)
-weight = tf.constant([[1, 1, 1]], dtype=tf.float32)
+# pred = tf.constant([[[0.2, 0.7], [0.6, 0.2], [0.2, 0.9]]], dtype=tf.float32)    # [[1, 0, 1]]
+pred1 = tf.constant([[[0.3, 0.7], [0.3, 0.7], [0.3, 0.7]]], dtype=tf.float32)    # [[1, 0, 1]]
+pred2 = tf.constant([[[0.1, 0.9], [0.1, 0.9], [0.1, 0.9]]], dtype=tf.float32)    # [[1, 0, 1]]
+weight = tf.constant([[1, 0, 0]], dtype=tf.float32)
 
 # seq_loss = tf.contrib.seq2seq.sequence_loss(logits, targets, weights)
-seq_loss = tf.contrib.seq2seq.sequence_loss(logits=pred, targets=y_data, weights=weight)
+seq_loss1 = tf.contrib.seq2seq.sequence_loss(logits=pred1, targets=y_data, weights=weight)
+seq_loss2 = tf.contrib.seq2seq.sequence_loss(logits=pred2, targets=y_data, weights=weight)
 with tf.Session() as sess:
+    seq_loss_val = sess.run([seq_loss1, seq_loss2])
+    print(seq_loss_val)     # 0.5967595, [0.5130153, 0.3711007]
--- lab12-2.py
+++ lab12-2.py
@@ -20,7 +20,7 @@ seq_length = 6
 input_dim = 5
 hidden_size = 5
 X = tf.placeholder(dtype=tf.float32, shape=[1, seq_length, input_dim])
-Y = tf.placeholder(dtype=tf.float32, shape=[1, seq_length])
+Y = tf.placeholder(dtype=tf.int32, shape=[1, seq_length])
 
 rnn_cell = tf.nn.rnn_cell.BasicRNNCell(num_units=hidden_size)
 initial_state = rnn_cell.zero_state(batch_size=batch_size, dtype=tf.float32)
@@ -29,3 +29,19 @@ initial_state = rnn_cell.zero_state(batch_size=batch_size, dtype=tf.float32)
 # sequence_loss가 어떻게 동작하는지는 lab12-1에서 해보는 중
 outputs, _states = tf.nn.dynamic_rnn(rnn_cell, X, initial_state=initial_state, dtype=tf.float32)
 
+weights = tf.ones([batch_size, seq_length])
+sequence_loss = tf.contrib.seq2seq.sequence_loss(logits=outputs, targets=Y, weights=weights)
+loss = tf.reduce_mean(sequence_loss)
+train = tf.train.AdamOptimizer(learning_rate=0.1).minimize(loss=loss)
+
+pred = tf.argmax(outputs, axis=2)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for step in range(10):
+        l, _ = sess.run([loss, train], feed_dict={X: x_onehot, Y: y_data})
+        result = sess.run(pred, feed_dict={X: x_onehot})
+        print(step, "loss: ", l, "prediction: ", result, "Y: ", y_data)
+
+        result_str = [id2char[c] for c in np.squeeze(result)]
+        print("\tPrediction string: ", ''.join(result_str))

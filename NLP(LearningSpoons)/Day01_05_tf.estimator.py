379932c608d59da390226f57a829aff7b037275b
hdh <scor9947@hanmail.net>
2019-08-29 12:57:45 +0900

    2'nd commit

--- NLP(LearningSpoons)/Day01_05_tf.estimator.py
+++ NLP(LearningSpoons)/Day01_05_tf.estimator.py
@@ -9,9 +9,10 @@ samples = ['너 오늘 이뻐 보인다',
            '나는 오늘 기분이 더러워',
            '끝내주는데, 좋은 일이 있나봐',
            '나 좋은 일이 생겼어',
+           '오늘 좋은 일이 생겼어',
            '아 오늘 진짜 짜증나',
            '환상적인데, 정말 좋은거 같아']
-targets = [[1], [0], [1], [1], [0], [1]]
+targets = [[1], [0], [1], [1], [1], [0], [1]]
 
 tokenizer = preprocessing.text.Tokenizer()
 tokenizer.fit_on_texts(samples)
@@ -48,6 +49,11 @@ def eval_input_fn():
 
 def pred_input_fn():
     dataset = tf.data.Dataset.from_tensor_slices(sequences)
+    dataset = dataset.batch(len(sequences))
+    # batch를 안하면 shape 에러가 뜨는데, 그냥 레코드 하나만 불러오나?, shape이 임베딩 크기라고 나온다.
+    # ValueError: Input 0 of layer dense is incompatible with the layer:
+    # : expected min_ndim=2, found ndim=1. Full shape received: [6]
+    # dataset = dataset.shuffle(len(sequences))
     return dataset
 
 
@@ -60,29 +66,44 @@ def model_fn(features, labels, mode):
     embed_input = tf.keras.layers.Embedding(vocab_size, emb_size)(features)
     embed_input = tf.reduce_mean(embed_input, axis=-1)
 
+    # with tf.Session() as sess:
+    #     print('embed_input: ', sess.run(embed_input))
+    # FailedPreconditionError (see above for traceback): GetNext() failed because the iterator has not been initialized.
+    # Ensure that you have run the initializer operation for this iterator before getting the next element.
+    # 	 [[node IteratorGetNext (defined at /home/hdh/Env/tensorflow-gpu-1.13/lib/python3.6/site-packages/tensorfl
+    # 	 ow_estimator/python/estimator/util.py:110) ]]
+
     hidden_layer = tf.keras.layers.Dense(128, activation=tf.nn.relu)(embed_input)
     output_layer = tf.keras.layers.Dense(1)(hidden_layer)
     output = tf.nn.sigmoid(output_layer)
-    # prediction = {'class_ids': tf.argmax(input=output, axis=1), 'logits': output}
+    # output = {'class': tf.cast(output, tf.int32)}
 
-    loss = tf.losses.mean_squared_error(output, labels)
+    # loss = tf.losses.mean_squared_error(output, labels)
 
-    eval_metric_op = {"accuracy": tf.metrics.accuracy(labels, output)}
     global_step = tf.train.get_global_step()  # 이거 하나가 새로 들어간건데,
-    train_op = tf.train.AdamOptimizer(1e-3).minimize(loss, global_step)
+    # train_op = tf.train.AdamOptimizer(1e-3).minimize(loss, global_step)
+
+    # eval_metric_op = {"accuracy": tf.metrics.accuracy(labels, output)}
 
     if mode == tf.estimator.ModeKeys.EVAL:
+        loss = tf.losses.mean_squared_error(output, labels)
         return tf.estimator.EstimatorSpec(mode=mode, loss=loss, predictions=output)
-    elif mode == tf.estimator.ModeKeys.TRAIN:
+
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        loss = tf.losses.mean_squared_error(output, labels)
+        train_op = tf.train.AdamOptimizer(1e-3).minimize(loss, global_step)
+        eval_metric_op = {"accuracy": tf.metrics.accuracy(labels, output)}
         return tf.estimator.EstimatorSpec(mode=mode, train_op=train_op, loss=loss,
                                           eval_metric_ops=eval_metric_op,
-                                          # predictions=output
                                           )
-    elif mode == tf.estimator.ModeKeys.PRED:
-        return tf.estimator.EstimatorSpec(mode=mode, train_op=train_op, loss=loss,
-                                          eval_metric_ops=eval_metric_op,
-                                          predictions=output_layer
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode,
+                                          predictions=output
                                           )
+
+    # return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_op)
+    # 출처: https://excelsior-cjh.tistory.com/157 [EXCELSIOR]
 
 
 DATA_OUT_PATH = './data_out/'
@@ -91,14 +112,19 @@ if not os.path.exists(DATA_OUT_PATH):
     os.makedirs(DATA_OUT_PATH)
 
 estimator = tf.estimator.Estimator(model_fn=model_fn, model_dir=DATA_OUT_PATH + 'checkpoint/dnn')
-estimator.train(train_input_fn)
-
+estimator.train(input_fn=train_input_fn)
 ev = estimator.evaluate(input_fn=eval_input_fn)
-# estimator.evaluate(train_input_fn)
-print('evaluation: ', ev)       # evaluation:  {'loss': 2.0537819e-07, 'global_step': 6570}
-# print('evaluation: ', ev['loss'])       # evaluation:  {'loss': 2.0537819e-07, 'global_step': 6570}
+pred = estimator.predict(input_fn=pred_input_fn)
 
-pred = estimator.predict(input_fn=train_input_fn)
-results = list(pred)
-# print(pred)
-tf.logging.info(results)
+# train_spec = tf.estimator.TrainSpec(input_fn=train_input_fn, max_steps=1000)
+# eval_spec = tf.estimator.EvalSpec(input_fn=eval_input_fn)
+# tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
+
+# estimator.train_and_evaluate(input_fn=train_input_fn)
+
+print('evaluation: ', ev)       # evaluation:  {'loss': 2.0537819e-07, 'global_step': 6570}
+print('pred: ', pred)
+print('pred: ', [int(round(item[0])) for item in list(pred)])
+# print('evaluation: ', ev['loss'])       # evaluation:  {'loss': 2.0537819e-07, 'global_step': 6570}
+# print('pred: ', pred)
+# tf.logging.info(results)
